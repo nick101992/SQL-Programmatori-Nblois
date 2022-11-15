4f00309ede5b47fc7450b90cf270728ebf8fb989
nick101992 <nick_1992@hotmail.it>
2022-11-15 17:13:10 +0100

    primi tre esercizi

--- esercizio.sql
+++ esercizio.sql
@@ -1,7 +1,8 @@
 /* 1. Calcolare il codice dei programmatori che sono stati autori di almeno un programma scritto in Java dopo il 2000. */
- SELECT *
-    FROM Programma
-    INNER JOIN Autore ON Programma.id = Autore.id
+SELECT Programmatore.codice
+    FROM Programmatore
+    JOIN Autore AS A ON Programmatore.codice = A.codice
+    JOIN Programma ON Programma.id = A.id
     WHERE Programma.linguaggio ='Java' AND Programma.anno > 2000
 
 /* 2. Calcolare il nome e la categoria dei programmatori che sono stati autori di almeno un programma scritto in un linguaggio diverso da Python, ordinando il risultato rispetto al nome dei programmatori. */
@@ -12,7 +13,14 @@ SELECT Programmatore.nome, Programmatore.categoria
     WHERE Programma.linguaggio !='Python'
     ORDER BY Programmatore.nome
     
-/* 3. Per ogni programmatore di categoria 10, calcolare il suo codice e l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java. */c
+/* 3. Per ogni programmatore di categoria 10, calcolare il suo codice e 
+l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java. */c
+
+SELECT Programmatore.codice, Programma.anno
+    FROM Programmatore
+    JOIN Autore AS A ON Programmatore.codice = A.codice
+    JOIN Programma ON Programma.id = A.id
+    WHERE Programmatore.categoria =10 AND Programma.linguaggio !='Java'
 
 /* 4. Calcolare le coppie dei codici di programmatori che sono stati coautori di almeno un programma scritto in Python. */
 
